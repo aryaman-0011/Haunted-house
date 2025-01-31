2f04b77a45408979110d0e921afd939e0956198a
Aryaman Kushwaha <kushwaha.aryaman21@gmail.com>
2025-01-31 21:38:43 +0530

    Created a group to add house components

--- src/script.js
+++ src/script.js
@@ -24,8 +24,14 @@ const floor = new THREE.Mesh(
     new THREE.PlaneGeometry(20, 20),
     new THREE.MeshStandardMaterial()
 )
+
 floor.rotation.x = - Math.PI * 0.5
 scene.add(floor)
+
+// House
+const house = new THREE.Group()
+scene.add(house)
+
 
 /**
  * Lights
