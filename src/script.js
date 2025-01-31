57b4657dc23b0bce4edfb264a47e65c44501b86e
Aryaman Kushwaha <kushwaha.aryaman21@gmail.com>
2025-01-31 21:35:15 +0530

    Removed Placeholder (Sphere)

--- src/script.js
+++ src/script.js
@@ -18,12 +18,6 @@ const scene = new THREE.Scene()
 /**
  * House
  */
-// Temporary sphere
-const sphere = new THREE.Mesh(
-    new THREE.SphereGeometry(1, 32, 32),
-    new THREE.MeshStandardMaterial({ roughness: 0.7 })
-)
-scene.add(sphere)
 
 // Floor
 const floor = new THREE.Mesh(
