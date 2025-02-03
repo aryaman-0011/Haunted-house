f715023d63fc97314ddce8e8c3ceced5a314201d
Aryaman Kushwaha <kushwaha.aryaman21@gmail.com>
2025-02-03 20:14:50 +0530

    Imported sky from three

--- src/script.js
+++ src/script.js
@@ -1,5 +1,6 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import { Sky } from 'three/addons/objects/Sky.js'
 import { Timer } from 'three/addons/misc/Timer.js'
 import GUI from 'lil-gui'
 
